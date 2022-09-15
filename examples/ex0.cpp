80da759755ca095136e49a37eb02365f613e8291
Jack <jack.allen@above-uav.com>
2022-09-15 00:48:29 -0700

    several changes more inline docs fix perfect forwarding in a reason ctor remove debug_str member function from error (want cleaner API) add debug namespace & str function imagine new template class (derror, defaultible error)

--- examples/ex0.cpp
+++ examples/ex0.cpp
@@ -1,88 +1,10 @@
 
-#include "jack/error.hpp"
-
-enum MyErrCode
-{
-    ParseError,
-    RPCError,
-};
-
-
-// class a
-// {
-//     constexpr a(const int code, const char* reason) :
-//             code(code), reason(reason) 
-//     {
-//         static_assert(code != 0, "what");
-//     }
-
-//     int code;
-//     const char* reason; 
-// };
-
-
-
-// jack::error returns_something_but_could_fail()
-// {
-//     // return jack::error::some(10, "what the");
-//     return jack::error::none();
-// }
-
-// jack::reason returns_nothing_but_may_fail()
-// {
-//     // return jack::reason::none();
-//     return jack::reason::some("abcd");
-// }
-
 #include <iostream>
-// #include "expected/include/tl/expected.hpp"
 
-// tl::expected<int, jack::error> foo()
-// {
-//     // return jack::error(10, "what_the_hell");
-//     return tl::make_unexpected(jack::error(101, "what the hell"));
-// }
-
-// #include "fake_optional.hpp"
-
-// optional<jack::reason> might_produce_reason()
-// {
-//     return jack::reason("what in the ", 101, "world");
-// }
-
-// optional<jack::error> might_produce_error()
-// {
-//     return jack::error(101, "ajsjjas", 1001);
-// }
+#include "jack/error.hpp"
 
 int main()
 {
-
-    // auto err = foo();
-    // if (err) {
-    //     printf("no err\n");
-    // } else {
-    //     auto e = std::move(err.error());
-    //     e.wrap("there was a problem");
-    //     auto re = std::move(e.reason());
-    //     printf("%s\n", re.c_str());
-    //     printf("%u\n", e.reason().size());
-    //     printf("%s\n", err.error().reason().c_str());
-    // }
-
-    // jack::error er(MyErrCode::RPCError, "failed to get number ", 11, " from board");
-
-
-    // printf("%s\n", er.reason().c_str());
-
-
-    // auto err = might_produce_error();
-    // if (err)
-    // {
-    //     (*err).extend("what", "the hell", 100)
-    // }
-
-
     jack::reason reason0("a fail reason");
     jack::reason reason1("a fail reason");
 
@@ -112,43 +34,6 @@ int main()
     error1.extend(jack::reason("reason ctx"));
     error1.extend("ap0, ", std::string("ap1, "), jack::reason("ap2, "), 100);
 
-    std::cout << error0.debug_string() << '\n'
-              << error1.debug_string() << '\n';
-
-
-    // std::cout << reason
-    //     .wrap(jack::reason("failed to do something"))
-    //     .extend("number is bad") << '\n';
-
-    // std::cout << jack::error(101, "a fail reason").debug_string() << '\n';
-
-    
-
-
-
-
-
-
-
-    // std::cout << sizeof(jack::reason) << '\n';
-    // // std::cout << sizeof(jack::error) << '\n';
-    // // std::cout << (void*)"" << '\n';
-    // // std::cout << sizeof(short) << '\n';
-    
-    // // a ana(10, "hello world");
-
-    // auto x = returns_nothing_but_may_fail()
-    //     .if_some([] (jack::reason* s) {
-
-    //     })
-    //     .if_none([] {
-
-    //     });
-
-
-    // std::cout << std::string("abcd").empty() << '\n';
-    // std::cout << std::string("").empty() << '\n';
-    
-
-    // int x = Code0;
+    std::cout << jack::debug::str(error0) << '\n'
+              << jack::debug::str(error1) << '\n';
 }
--- include/jack/error.hpp
+++ include/jack/error.hpp
@@ -42,7 +42,7 @@ inline void variadic_strcat(std::stringstream& stream,
  * @brief Construct a string from an arbitrary series of parameters
  * using a std::stringstream and operator<<.
  * 
- * @return std::string 
+ * @return std::string constructed from given args
  */
 template <typename... str_ts>
 inline std::string make_str(str_ts&&... args)
@@ -54,14 +54,17 @@ inline std::string make_str(str_ts&&... args)
 }
 
 /**
- * @brief Eat empty make_str calls w/o the fluff.
+ * @brief No-argument overload of detail::make_str that avoids
+ * useless calls when returning an empty std::string.
+ * 
+ * @return empty std::string
  */
 inline std::string make_str()
 {
     return {};
 }
 
-#endif // #ifndef JACK_DETAIL_VARIADIC_MAKE_STR
+#endif // #ifndef
 
 } // namespace detail
 
@@ -75,7 +78,9 @@ class reason : private std::string
     // Expose std::string member functions 
     using std::string::c_str;
 
-    /// @brief prevent default reason construction (does a default reason make any sense?)
+    /**
+     * @brief Prevent default reason construction.
+     */
     reason() = delete;
 
     /**
@@ -132,7 +137,8 @@ class reason : private std::string
      * @param str values to construct a reason from
      */
     template <typename... str_args>
-    reason(str_args... str) : std::string(detail::make_str(str...))
+    reason(str_args... str) : std::string(detail::make_str(
+            std::forward<str_args>(str)...))
     {
     }
 
@@ -140,7 +146,7 @@ class reason : private std::string
      * @brief Copy assignment operator.
      * 
      * @param other reason to copy from
-     * @return reason& to this reason
+     * @return reference to this reason
      */
     reason& operator=(const reason& other) = default;
     
@@ -148,7 +154,7 @@ class reason : private std::string
      * @brief Move assignment operator.
      * 
      * @param from reason to move from
-     * @return reason& to this reason
+     * @return reference to this reason
      */
     reason& operator=(reason&& from) = default;
 
@@ -156,6 +162,7 @@ class reason : private std::string
      * @brief Wrap this reason with additional context (prepend).
      * 
      * @param context values to contruct a string from
+     * @return reference to this reason
      */
     template <typename... str_args>
     reason& wrap(str_args... context)
@@ -171,6 +178,7 @@ class reason : private std::string
      * @brief Wrap this reason with additional context (prepend).
      * 
      * @param context c string to copy from
+     * @return reference to this reason
      */
     reason& wrap(const char* context)
     {
@@ -183,6 +191,7 @@ class reason : private std::string
      * @brief Wrap this reason with additional context (prepend).
      * 
      * @param context std::string to copy from
+     * @return reference to this reason
      */
     reason& wrap(const std::string& context)
     {
@@ -195,6 +204,7 @@ class reason : private std::string
      * @brief Wrap this reason with additional context (prepend).
      * 
      * @param context reason to copy from
+     * @return reference to this reason
      */
     reason& wrap(const reason& context)
     {
@@ -207,6 +217,7 @@ class reason : private std::string
      * @brief Extend this reason with additional information (append).
      * 
      * @param info values to construct a string from
+     * @return reference to this reason
      */
     template <typename... str_args>
     reason& extend(str_args... info)
@@ -222,6 +233,7 @@ class reason : private std::string
      * @brief Extend this reason with additional information (append).
      * 
      * @param info c string to copy from
+     * @return reference to this reason
      */
     reason& extend(const char* info)
     {
@@ -234,6 +246,7 @@ class reason : private std::string
      * @brief Extend this reason with additional information (append).
      * 
      * @param info std::string to copy from
+     * @return reference to this reason
      */
     reason& extend(const std::string& info)
     {
@@ -246,6 +259,7 @@ class reason : private std::string
      * @brief Extend this reason with additional information (append).
      * 
      * @param info reason to copy from
+     * @return reference to this reason
      */
     reason& extend(const reason& info)
     {
@@ -255,7 +269,13 @@ class reason : private std::string
     }
 };
 
-/// @brief Implement stream operator for reason class
+/**
+ * @brief Implement stream operator for reason class.
+ * 
+ * @param os std::ostream reference to write to
+ * @param reason reason to write from
+ * @return reference to param os
+ */
 inline std::ostream& operator<<(std::ostream& os, const reason& reason)
 {
     return os << reason.c_str();
@@ -268,42 +288,117 @@ inline std::ostream& operator<<(std::ostream& os, const reason& reason)
 class error
 {
   public:
-    error(error&& other) = default;
-    error(const error& other) = default;
 
+    /**
+     * @brief Prevent default error construction.
+     */
+    error() = delete;
+
+    /**
+     * @brief Construct a new error object by moving from
+     * another error object.
+     * 
+     * @param other error to move from
+     */
+    error(error&& other) = default;
+    
+    /**
+     * @brief Construct a new error object by copying from
+     * another error object.
+     * 
+     * @param other error to copy from
+     */
+    error(const error& other) = default;
+    
+    /**
+     * @brief Construct a new error object by accepting a 
+     * code and a reason object to copy from.
+     * 
+     * @param code error code
+     * @param reason reason to copy from
+     */
     error(int code, const reason& reason) :
             code(code), desc(reason)
     {
     }
 
+    /**
+     * @brief Construct a new error object by accepting a 
+     * code and a reason object to move from.
+     * 
+     * @param code error code
+     * @param reason reason to move from
+     */
     error(int code, reason&& reason) :
             code(code), desc(std::move(reason))
     {
     }
-
+    
+    /**
+     * @brief Construct a new error object by accepting a 
+     * code and a c string to copy from.
+     * 
+     * @param code error code
+     * @param reason c string to copy from
+     */
     error(int code, const char* reason) :
             code(code), desc(reason)
     {
     }
 
+    /**
+     * @brief Construct a new error object by accepting a 
+     * code and a std::string to copy from.
+     * 
+     * @param code error code
+     * @param reason std::string to copy from
+     */
     error(int code, const std::string& reason) :
             code(code), desc(reason)
     {
     }
-
+    
+    /**
+     * @brief Construct a new error object by accepting a 
+     * code and a std::string to move from.
+     * 
+     * @param code error code
+     * @param reason std::string to move from
+     */
     error(int code, std::string&& reason) : 
             code(code), desc(std::move(reason))
     {
     }
-
+    
+    /**
+     * @brief Construct a new error object from an arbitrary series
+     * of parameters using a std::stringstream and operator<<.
+     * 
+     * @tparam str_args types of arguments used to construct the reason
+     * @param code error code
+     * @param reason values to construct a reason from
+     */
     template <typename... str_args>
     error(int code, str_args... reason) :
             code(code), desc(reason...)
     {
     }
+    
+    /**
+     * @brief Copy assignment operator.
+     * 
+     * @param other error to copy from
+     * @return reference to this error
+     */
+    error& operator=(const error& other) = default;
 
-    error& operator=(error&&) = default;
-    error& operator=(const error&) = default;
+    /**
+     * @brief Move assignment operator.
+     * 
+     * @param from error to move from
+     * @return reference to this error
+     */
+    error& operator=(error&& from) = default;
 
     /**
      * @brief Wrap this error's reason with additional context (prepend).
@@ -329,21 +424,38 @@ class error
         return *this;
     }
 
-    /**
-     * @brief Produce a human-readable debug string of this error.
-     */
-    std::string debug_string() const
-    {
-        return detail::make_str("error { code: ", code,
-                ", desc: \"", desc, "\" }");
-    }
-
-    /// @brief 
+    /// @brief Signed integer error code.
     int code;
 
-    /// @brief 
+    /// @brief Human-readable error description.
     reason desc;
 };
+
+/**
+ * @brief A human-readable error description with a
+ * paired code for programmatic error handling. 
+ * 
+ * @tparam default integer to represent default state
+ */
+// template <int default>
+// class derror;
+
+namespace debug
+{
+
+/**
+ * @brief Produce a friendly debug string.
+ * 
+ * @param error error to copy from
+ * @return debug string from given error
+ */
+inline std::string str(const error& error)
+{
+    return detail::make_str("error { code: ", error.code,
+            ", desc: \"", error.desc, "\" }");
+}
+
+} // namespace debug
 
 } // namespace jack
 
